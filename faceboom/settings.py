99c1d753f0ebb3739029af28b432417f5c36e553
unknown <parsashakeri614@gmail.com>
2025-11-13 15:22:09 +0330

    make first ORM (User)

--- faceboom/settings.py
+++ faceboom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=fupzqbh^(*e45v+%-9xjkvnb5msnxat*)jdwcaosaqkbt^do#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -102,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
